4aae828302fe7e3c4110c23210d6f4f9b3a628d4
tmaounis <timothymaounis@Timothys-MacBook-Pro.local>
2017-11-08 18:32:02 -0500

    tabs

--- ProjectX/contracts/Coin.sol
+++ ProjectX/contracts/Coin.sol
@@ -4,31 +4,31 @@ pragma solidity ^0.4.0;
 
 contract Coin {
 
-address public minter; 
-mapping (address => uint256) public balances; 
-	
-//logging of sending activity 
-event Sent(address from, address to, uint amount);
+	address public minter; 
+	mapping (address => uint256) public balances; 
+		
+	//logging of sending activity 
+	event Sent(address from, address to, uint amount);
 
-function Coin() {
-	minter = msg.sender;
-}
+	function Coin() {
+		minter = msg.sender;
+	}
 
-function mint(address receiver, uint amount) {
-	if (msg.sender != minter) return;
-	balances[receiver] += amount; 
-}
+	function mint(address receiver, uint amount) {
+		if (msg.sender != minter) return;
+		balances[receiver] += amount; 
+	}
 
-function destroy(address receiver, uint amount) {
-	if(msg.sender != minter) return; 
-	balances[receiver] -= amount; 
-}
+	function destroy(address receiver, uint amount) {
+		if(msg.sender != minter) return; 
+		balances[receiver] -= amount; 
+	}
 
-function send(address receiver, uint amount) {
-	if (balances[msg.sender] < amount) return;
-	balances[msg.sender] -= amount; 
-	balances[receiver] += amount; 
-	Sent(msg.sender, receiver, amount); 
-}
+	function send(address receiver, uint amount) {
+		if (balances[msg.sender] < amount) return;
+		balances[msg.sender] -= amount; 
+		balances[receiver] += amount; 
+		Sent(msg.sender, receiver, amount); 
+	}
 
 }
